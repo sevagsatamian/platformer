d6a469e9e64abbdbfcb6a4232aee93e7ea66af6b
sevagsatamian <.comsevagsatamian@gmail.com>
2014-05-14 21:43:34 -0700

    added enemy

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -125,3 +125,74 @@ game.CoinEntity = me.CollectableEntity.extend({
 	}
 
 });
+game.EnemyEntity = game.CharacterEntity.extend({
+	init:function(x, y, settings) {
+		settings.image = 'bad1';
+		settings.spritewidth = 32;
+		this.parent(x, y, settings);
+
+		this.hp = 1;
+
+		this.dmg = 1;
+		this.aggroed = false;
+		this.firstCheck = true;
+		this.walkLeft = false;
+
+		this.startX = x;
+		this.endX = x + settings.width - settings.spritewidth;
+
+		this.setVelocity(2, 6);
+
+		this.type = me.game.ENEMY_OBJECT;
+	},
+
+	checkLOS: function() {
+
+		// This is so we only check through the entities list one time, not every frame
+		if(this.firstCheck) {
+			this.player = me.game.getEntityByName("player")[0];
+			this.firstCheck = false;
+		}
+
+		if(!this.walkLeft) {
+			if(((this.pos.x - this.player.pos.x <= 100) && (this.pos.x - this.player.pos.x >= 0)) &&
+				(this.pos.y - this.player.pos.y <= 150) && (this.pos.y - this.player.pos.y >= 0)) {
+					this.vel.x += -this.accel.x * me.timer.tick;
+					this.walkLeft = true;
+					return true;
+			}
+		} else {
+			if (((this.pos.x - this.player.pos.x >= -100) && (this.pos.x - this.player.pos.x <= 0)) &&
+				(this.pos.y - this.player.pos.y <= 150) && (this.pos.y - this.player.pos.y >= 0)) {
+					this.vel.x += this.accel.x * me.timer.tick;
+					this.walkLeft = false;
+					return true;
+			}
+		}
+		this.flipX(this.walkLeft);
+		return false;
+	},
+
+	// This just moves it back and forth
+	patrol: function() {
+
+		if(this.walkLeft && this.pos.x <= this.startX) {
+			this.walkLeft = false;
+		} else if (!this.walkLeft && this.pos.x >= this.endX) {
+			this.walkLeft = true;
+		}
+
+		this.flipX(this.walkLeft);
+		this.vel.x += (this.walkLeft) ? -this.accel.x * me.timer.tick : this.accel.x * me.timer.tick;
+
+	},
+
+	getMovements: function() {
+
+		this.vel.x = 0; //change velocity every update; maybe dumb
+		this.aggroed = this.checkLOS();
+		if(!this.aggroed) {
+			this.patrol();
+		} 
+	}
+});
